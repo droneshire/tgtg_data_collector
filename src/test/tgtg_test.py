0daadbbde9eb4528a9291aae2aece4406cd4383a
Ross Yeager <ryeager12@gmail.com>
2023-09-09 13:49:30 -0700

    wip

--- src/test/tgtg_test.py
+++ src/test/tgtg_test.py
@@ -24,7 +24,7 @@ from util import log
 
 class TgtgTest(unittest.TestCase):
     test_email: str = "test@gmail.com"
-    verbose: bool = True
+    verbose: bool = False
     time_zone = pytz.timezone("America/Los_Angeles")
     time_zone_other = pytz.timezone("America/New_York")
     test_data_dir = os.path.join(os.path.dirname(__file__), "test_data")
