df9f9922d8b254d155bbe5b2362ed70d460f7160
Ross Yeager <ryeager12@gmail.com>
2023-10-22 23:14:40 -0700

    working scraper dog

--- src/util/proxies.py
+++ src/util/proxies.py
@@ -23,12 +23,9 @@ class ScrapeDogProxy(Proxies):
         dotenv.load_dotenv()
         api_key = os.getenv("SCRAPER_DOG_PROXY_API_KEY")
         assert api_key is not None, "Missing SCRAPER_DOG_PROXY_API_KEY in .env"
-        self.proxy_url = {
-            "http:": f"http://scrapingdog:{api_key}-country=us@proxy.scrapingdog.com:8081"
-        }
+        self.proxy_url = {"http": f"http://scrapingdog:{api_key}@proxy.scrapingdog.com:8081"}
 
     def get(self):
-        raise Exception("Scraper Dog does not work as proxy param, need to wrap requests")
         log.print_bright(f"Using proxy: {self.proxy_url}")
         return self.proxy_url
 
