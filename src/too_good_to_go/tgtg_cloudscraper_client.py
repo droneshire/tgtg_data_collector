25ddc40ca19e2a7dce1947caf5e43db2665a8625
Ross Yeager <ryeager12@gmail.com>
2023-10-22 23:21:18 -0700

    cleanup

--- src/too_good_to_go/tgtg_cloudscraper_client.py
+++ src/too_good_to_go/tgtg_cloudscraper_client.py
@@ -46,8 +46,5 @@ class TgtgCloudscraperClient(TgtgClient):
         self.session = cloudscraper.session()
         self.session.headers = super()._headers
 
-    def reset_session(self, proxies: T.Dict[str, str]) -> T.Dict[str, str]:
-        if self.proxies == proxies:
-            return self.proxies
-        log.print_bright(f"Resetting session with new proxy: {proxies}")
+    def reset_session(self, proxies: T.Dict[str, str]) -> None:
         self.proxies = proxies
