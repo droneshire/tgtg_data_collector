42b9e4a32d98bc2d70e7fd34dc122fdb34ad4d54
Ross Yeager <ryeager12@gmail.com>
2024-02-16 20:54:35 -0800

    add more helpers

--- src/demographics/google_places.py
+++ src/demographics/google_places.py
@@ -1,4 +1,12 @@
+"""
+Google Places API wrapper
+
+https://developers.google.com/maps/documentation/places/web-service/overview
+https://developers.google.com/maps/documentation/places/web-service/text-search
+"""
+
 import copy
+import json
 import os
 import typing as T
 
@@ -15,17 +23,29 @@ class GooglePlacesAPI:
     }
     DEFAULT_FIELDS = "places.formattedAddress,places.displayName"
 
-    def __init__(self, api_key) -> None:
+    def __init__(self, api_key: str, verbose: bool = False) -> None:
         self.api_key = api_key
         self.HEADERS["X-Goog-Api-Key"] = api_key
         self.base_url = "https://places.googleapis.com/v1"
+        self.verbose = verbose
 
-    def search_places(
-        self, query: str, fields: T.Optional[T.List[str]] = None
+    def text_search(
+        self,
+        query: str,
+        fields: T.Optional[T.List[str]] = None,
+        data: T.Optional[T.Dict[str, T.Any]] = None,
     ) -> T.Dict[T.Any, T.Any]:
         json_data = {
             "textQuery": query,
         }
+
+        if data:
+            json_data.update(data)
+
+        if self.verbose:
+            log.print_normal(f"Searching for {query}")
+            log.print_normal(f"Data: {json.dumps(json_data, indent=2)}")
+
         headers = copy.deepcopy(self.HEADERS)
 
         headers["X-Goog-FieldMask"] = ",".join(fields) if fields else self.DEFAULT_FIELDS
@@ -43,3 +63,34 @@ class GooglePlacesAPI:
             log.print_fail(f"Could not search for {query}")
             log.print_warn(exception)
             return {}
+
+    def search_location_radius(
+        self,
+        latitude: float,
+        longitude: float,
+        radius_miles: float,
+        query: str,
+        fields: T.Optional[T.List[str]] = None,
+        included_type: T.Optional[str] = None,
+    ) -> T.Dict[T.Any, T.Any]:
+        radius_meters = min(radius_miles * 1609.34, 50000.0)
+
+        if self.verbose:
+            log.print_normal(
+                f"Searching for {query} within {radius_miles} miles of {latitude}, {longitude}"
+            )
+        json_data: T.Dict[str, T.Any] = {
+            "locationBias": {
+                "circle": {
+                    "center": {"latitude": latitude, "longitude": longitude},
+                    "radius": radius_meters,
+                }
+            },
+        }
+
+        if included_type is not None:
+            json_data["includedType"] = included_type
+
+        if self.verbose:
+            log.print_normal(f"Data: {json.dumps(json_data, indent=2)}")
+        return self.text_search(query=query, fields=fields, data=json_data)
