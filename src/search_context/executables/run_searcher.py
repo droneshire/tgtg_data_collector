c1c20aae96a8ccc8f55cb84dbd44530fdb20aff3
Ross Yeager <ryeager12@gmail.com>
2024-03-01 23:05:53 -0800

    searcher class and executable

---
+++ src/search_context/executables/run_searcher.py
@@ -0,0 +1,156 @@
+"""
+Executable that runs the searcher on a grid search.
+"""
+
+import argparse
+import os
+import typing as T
+from datetime import datetime
+
+import dotenv
+import pytz
+
+from constants import PROJECT_NAME
+from search_context.google_places import GooglePlacesAPI
+from search_context.searcher import MAX_SEARCH_CALLS, Searcher
+from search_context.util import (
+    METERS_PER_MILE,
+    SearchGrid,
+    get_city_center_coordinates,
+    get_search_grid_details,
+)
+from util import log, file_util
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Run the searcher on a grid search")
+
+    firebase_credentials_file = os.environ["FIREBASE_CREDENTIALS_FILE"]
+    firebase_storage_path = os.environ["FIREBASE_STORAGE_PATH"]
+    census_api_key = os.environ["CENSUS_API_KEY"]
+    google_places_api_key = os.environ["GOOGLE_MAPS_PLACES_API_KEY"]
+    test_email = os.environ["TEST_EMAIL_ADDRESS"]
+
+    parser.add_argument(
+        "--census_api_key", type=str, default=census_api_key, help="US Census API key"
+    )
+    parser.add_argument(
+        "--google-api_key", type=str, default=google_places_api_key, help="Google Places API key"
+    )
+    datetime_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    log_dir = log.get_logging_dir(PROJECT_NAME)
+    results_dir = os.path.join(log_dir, "search_context_results")
+    file_util.make_sure_path_exists(results_dir)
+    default_results_csv = os.path.join(results_dir, f"search_results_{datetime_str}.csv")
+    parser.add_argument(
+        "--results_csv",
+        type=str,
+        default=default_results_csv,
+        help="CSV file to write results to",
+    )
+    parser.add_argument(
+        "--email", type=str, default=test_email, help="Email address to send notifications to"
+    )
+    parser.add_argument(
+        "--credentials_file",
+        type=str,
+        default=firebase_credentials_file,
+        help="Firebase credentials file",
+    )
+    parser.add_argument(
+        "--storage_bucket",
+        type=str,
+        default=firebase_storage_path,
+        help="Firebase storage bucket",
+    )
+    parser.add_argument(
+        "--max_search_calls",
+        type=int,
+        default=MAX_SEARCH_CALLS,
+        help="Maximum number of search calls",
+    )
+    parser.add_argument(
+        "--clamp_at_max",
+        action="store_true",
+        default=True,
+        help="Clamp the number of search calls at the maximum",
+    )
+    parser.add_argument(
+        "--radius_miles", type=float, default=20.0, help="Radius in miles to search"
+    )
+    parser.add_argument("--city", type=str, required=True, help="City to search in")
+    parser.add_argument("--max_cost", type=float, default=10.0, help="Maximum cost")
+    parser.add_argument("--cost_per_search", type=float, default=0.04, help="Cost per search")
+    parser.add_argument("--verbose", action="store_true", help="Verbose output")
+    parser.add_argument("--dry_run", action="store_true", help="Dry run")
+    return parser.parse_args()
+
+
+def get_search_grid(
+    city: str, radius_miles: float, max_cost: float, cost_per_search: float, google_api_key: str
+) -> T.List[SearchGrid]:
+    radius_meters = radius_miles * METERS_PER_MILE
+
+    city_center_coordinates = get_city_center_coordinates(city)
+    assert city_center_coordinates, f"Location not found for {city}"
+
+    center_lat, center_lon = city_center_coordinates
+
+    # Get the maximum width of the viewport for our search to have good resolution
+    # since places api limits the search results to 20 max regardless of the radius
+    google_places = GooglePlacesAPI(google_api_key, verbose=True)
+    log.print_bright("Finding maximum viewpoint width...")
+    max_grid_resolution_width_meters = google_places.find_maximum_viewpoint_width(
+        center_lat, center_lon, "All restaurants"
+    )
+
+    log.print_ok_blue(f"Maximum viewpoint width: {max_grid_resolution_width_meters} meters")
+
+    grid, city_center_coordinates, num_grid_squares, total_cost = get_search_grid_details(
+        city,
+        max_grid_resolution_width_meters,
+        radius_meters,
+        max_cost,
+        cost_per_search,
+        verbose=True,
+    )
+
+    log.print_ok_blue(f"Number of grid squares: {num_grid_squares}")
+    log.print_ok_blue(f"Total cost: ${total_cost:.2f}")
+    log.print_ok_blue(f"City center: {city_center_coordinates}")
+    log.print_ok_blue(f"Grid resolution: {max_grid_resolution_width_meters} meters")
+
+    return grid
+
+
+def main() -> None:
+    dotenv.load_dotenv()
+
+    args = parse_args()
+
+    searcher = Searcher(
+        google_api_key=args.google_api_key,
+        us_census_api_key=args.census_api_key,
+        results_csv=args.results_csv,
+        email=args.email,
+        credentials_file=args.credentials_file,
+        storage_bucket=args.storage_bucket,
+        max_search_calls=args.max_search_calls,
+        clamp_at_max=args.clamp_at_max,
+        verbose=args.verbose,
+    )
+
+    grid = get_search_grid(
+        args.city, args.radius_miles, args.max_cost, args.cost_per_search, args.google_api_key
+    )
+
+    searcher.run_search(
+        search_name=f"{args.city} Search",
+        search_grid=grid,
+        time_zone=pytz.timezone("America/Los_Angeles"),
+        dry_run=args.dry_run,
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/search_context/searcher.py
@@ -0,0 +1,181 @@
+import typing as T
+from datetime import datetime
+
+from rich.progress import BarColumn, Progress, TextColumn, TimeElapsedColumn
+
+from firebase.storage import FirebaseStorage
+from firebase.user import FirebaseUser
+from search_context.google_places import ADVANCED_FIELDS, ADVANCED_PROMPT, GooglePlacesAPI
+from search_context.us_census import USCensusAPI
+from search_context.util import SearchGrid
+from util import csv_logger, log
+from util.dict_util import safe_get
+
+MAX_SEARCH_CALLS = 20000
+
+
+class Searcher:
+    # pylint: disable=too-many-arguments
+    def __init__(
+        self,
+        google_api_key: str,
+        us_census_api_key: str,
+        results_csv: str,
+        email: str,
+        credentials_file: str,
+        storage_bucket: str,
+        max_search_calls: int = MAX_SEARCH_CALLS,
+        clamp_at_max: bool = False,
+        verbose: bool = False,
+    ) -> None:
+        self.google_places = GooglePlacesAPI(google_api_key, verbose=verbose)
+        self.us_census = USCensusAPI(us_census_api_key)
+        self.email = email
+        self.max_search_calls = max_search_calls
+        self.clamp_at_max = clamp_at_max
+        self.verbose = verbose
+
+        self.firestore_storage = FirebaseStorage(credentials_file, storage_bucket, verbose=verbose)
+        self.firestore_user: FirebaseUser = FirebaseUser(
+            credentials_file=credentials_file, send_email_callback=None
+        )
+        header = list(self._get_flatten_data("", "", {}).keys())
+        self.csv = csv_logger.CsvLogger(csv_file=results_csv, header=header)
+
+    def _get_flatten_data(self, search_name: str, timestamp: str, data: T.Dict) -> T.Dict:
+        flattened_data = {
+            "timestamp": timestamp,
+            "search_name": search_name,
+            "nationalPhoneNumber": safe_get(data, ["nationalPhoneNumber"], ""),
+            "formattedAddress": safe_get(data, ["formattedAddress"], ""),
+            "latitude": safe_get(data, ["location", "latitude"]),
+            "longitude": safe_get(data, ["location", "longitude"]),
+            "rating": safe_get(data, ["rating"]),
+            "googleMapsUri": safe_get(data, ["googleMapsUri"], ""),
+            "websiteUri": safe_get(data, ["websiteUri"], ""),
+            "openNow": safe_get(data, ["regularOpeningHours", "openNow"]),
+            "businessStatus": safe_get(data, ["businessStatus"], ""),
+            "userRatingCount": safe_get(data, ["userRatingCount"]),
+            "displayName": safe_get(data, ["displayName", "text"], ""),
+            "primaryTypeDisplayName": safe_get(data, ["primaryTypeDisplayName", "text"], ""),
+            "takeout": safe_get(data, ["takeout"]),
+            "delivery": safe_get(data, ["delivery"]),
+            "dineIn": safe_get(data, ["dineIn"]),
+            "servesBreakfast": safe_get(data, ["servesBreakfast"]),
+            "primaryType": safe_get(data, ["primaryType"]),
+            "editorialSummary": safe_get(data, ["editorialSummary", "text"], ""),
+            "acceptsCreditCards": safe_get(data, ["paymentOptions", "acceptsCreditCards"]),
+            "acceptsCashOnly": safe_get(data, ["paymentOptions", "acceptsCashOnly"]),
+            "wheelchairAccessibleParking": safe_get(
+                data, ["accessibilityOptions", "wheelchairAccessibleParking"]
+            ),
+            "wheelchairAccessibleEntrance": safe_get(
+                data, ["accessibilityOptions", "wheelchairAccessibleEntrance"]
+            ),
+            "wheelchairAccessibleRestroom": safe_get(
+                data, ["accessibilityOptions", "wheelchairAccessibleRestroom"]
+            ),
+            "wheelchairAccessibleSeating": safe_get(
+                data, ["accessibilityOptions", "wheelchairAccessibleSeating"]
+            ),
+        }
+
+        for i in range(7):  # Assuming 7 days a week
+            flattened_data[f"open_day_{i}"] = safe_get(
+                data, ["regularOpeningHours", "periods", i, "open", "day"]
+            )
+            flattened_data[f"open_hour_{i}"] = safe_get(
+                data, ["regularOpeningHours", "periods", i, "open", "hour"]
+            )
+            flattened_data[f"open_minute_{i}"] = safe_get(
+                data, ["regularOpeningHours", "periods", i, "open", "minute"]
+            )
+            flattened_data[f"close_day_{i}"] = safe_get(
+                data, ["regularOpeningHours", "periods", i, "close", "day"]
+            )
+            flattened_data[f"close_hour_{i}"] = safe_get(
+                data, ["regularOpeningHours", "periods", i, "close", "hour"]
+            )
+            flattened_data[f"close_minute_{i}"] = safe_get(
+                data, ["regularOpeningHours", "periods", i, "close", "minute"]
+            )
+
+        for i, desc in enumerate(
+            safe_get(data, ["regularOpeningHours", "weekdayDescriptions"], [])
+        ):
+            flattened_data[f"weekday_desc_{i}"] = desc
+
+        return flattened_data
+
+    def run_search(
+        self,
+        search_name: str,
+        search_grid: T.List[SearchGrid],
+        time_zone: T.Any,
+        prompt: str = ADVANCED_PROMPT,
+        fields: T.Optional[T.List[str]] = None,
+        dry_run: bool = False,
+    ) -> None:
+        log.print_bright("Starting search")
+
+        if fields is None:
+            fields = ADVANCED_FIELDS
+
+        if len(search_grid) == 0:
+            log.print_warn("No search grid provided")
+            return
+
+        if len(search_grid) > MAX_SEARCH_CALLS:
+            log.print_fail_arrow("Search grid larger than maximum allowed")
+
+            if not self.clamp_at_max:
+                return
+
+            log.print_warn(f"Clamping search grid at maximum of {MAX_SEARCH_CALLS}")
+            search_grid = search_grid[:MAX_SEARCH_CALLS]
+
+        with Progress(
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(bar_width=100),
+            TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
+            TextColumn("[progress.completed]{task.completed}/{task.total}"),
+            TimeElapsedColumn(),
+        ) as progress:
+            task = progress.add_task("Grid Search", total=len(search_grid))
+
+            for grid in search_grid:
+                data = {"locationRestriction": {"rectangle": grid["viewport"]}}
+
+                date_now = datetime.now()
+                date_localized = time_zone.localize(date_now)
+                local_offset = date_localized.utcoffset()
+                local_time = date_localized - local_offset
+
+                date_formated = local_time.strftime("%Y-%m-%d %H:%M:%S %Z")
+
+                try:
+                    if dry_run:
+                        log.print_bright(
+                            f"Dry run: Would be searching for "
+                            f"{grid['center']['latitude']}, {grid['center']['longitude']}"
+                        )
+                        progress.update(task, advance=1)
+                        continue
+                    results = self.google_places.text_search(prompt, fields, data)
+                except Exception as exception:  # pylint: disable=broad-except
+                    log.print_fail(
+                        f"Could not search for "
+                        f"{grid['center']['latitude']}, {grid['center']['longitude']}"
+                    )
+                    log.print_warn(exception)
+                    continue
+
+                if "places" not in results:
+                    log.print_warn("No results found")
+                else:
+                    for place in results["places"]:
+                        self.csv.write(
+                            self._get_flatten_data(search_name, date_formated, dict(place))
+                        )
+
+                progress.update(task, advance=1)
