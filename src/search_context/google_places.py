3747a876842a5180f54c291fea1a702a6bc077ba
Ross Yeager <ryeager12@gmail.com>
2024-04-07 23:03:51 -0700

    googleapi: add in more stuff

--- src/search_context/google_places.py
+++ src/search_context/google_places.py
@@ -3,6 +3,7 @@ Google Places API wrapper
 
 https://developers.google.com/maps/documentation/places/web-service/overview
 https://developers.google.com/maps/documentation/places/web-service/text-search
+https://developers.google.com/maps/documentation/places/web-service/search
 """
 
 import copy
@@ -12,7 +13,7 @@ import typing as T
 
 import requests
 
-from search_context.util import get_viewport
+from search_context.util import Coordinates, get_viewport
 from util import log
 
 ADVANCED_FIELDS = [
@@ -52,7 +53,100 @@ TYPES = [
     "point_of_interest",
     "establishment",
 ]
-ADVANCED_PROMPT = f"All {'s, '.join(list(t for t in TYPES))}"
+
+
+def call_api(
+    url: str,
+    headers: T.Optional[T.Dict[str, T.Any]] = None,
+    params: T.Optional[T.Dict[str, T.Any]] = None,
+    json_data: T.Optional[T.Dict[str, T.Any]] = None,
+    timeout: float = 10.0,
+) -> T.Dict[T.Any, T.Any]:
+    headers = headers or {}
+    json_data = json_data or {}
+    params = params or {}
+
+    try:
+        response = requests.post(
+            url, headers=headers, params=params, json=json_data, timeout=timeout
+        ).json()
+        if not isinstance(response, dict):
+            print(f"Failed results from {url}")
+            print(response)
+            return {}
+
+        return response
+    except Exception as exception:  # pylint: disable=broad-except
+        print(f"Failed results for {url}")
+        print(exception)
+        raise exception
+
+
+class GoogleMapsAPI:
+    def __init__(self, api_key: str, verbose: bool = False) -> None:
+        self.api_key = api_key
+        self.base_url = "https://maps.googleapis.com/maps/api"
+        self.verbose = verbose
+
+    def find_place_from_location(self, place: str, location: Coordinates) -> T.Dict[T.Any, T.Any]:
+        location_string = f"{location['latitude']},{location['longitude']}"
+        params = {
+            "input": place,
+            "inputtype": "textquery",
+            "fields": "name,place_id",
+            "locationbias": f"point:{location_string}",
+            "key": self.api_key,
+        }
+
+        url = os.path.join(self.base_url, "place", "findplacefromtext", "json")
+
+        if self.verbose:
+            print(f"Searching for {place} at {location_string}")
+
+        return call_api(url, params=params)
+
+    def nearby_search(
+        self,
+        location: Coordinates,
+        radius_meters: int,
+        location_type: T.List[str],
+        keyword: str,
+    ) -> T.Dict[T.Any, T.Any]:
+        location_string = f"{location['latitude']},{location['longitude']}"
+        assert location_type in TYPES, f"Invalid location type {location_type}"
+
+        params = {
+            "location": location_string,
+            "radius": radius_meters,
+            "type": location_type,
+            "keyword": keyword,
+            "key": self.api_key,
+        }
+
+        url = os.path.join(self.base_url, "place", "nearbysearch", "json")
+
+        if self.verbose:
+            print(f"Searching for {keyword} at {location_string}")
+
+        return call_api(url, params=params)
+
+    def details_from_place_id(
+        self, place_id: str, fields: T.Optional[T.List[str]] = None
+    ) -> T.Dict[T.Any, T.Any]:
+        fields = fields or [field.replace("places.", "") for field in ADVANCED_FIELDS]
+
+        params = {
+            "place_id": place_id,
+            "fields": ",".join(fields),
+            "key": self.api_key,
+        }
+
+        url = os.path.join(self.base_url, "place", "details", "json")
+
+        if self.verbose:
+            print(f"Getting details for {place_id}")
+
+        return call_api(url, params=params)
 
 
 class GooglePlacesAPI:
@@ -94,18 +188,25 @@ class GooglePlacesAPI:
         headers["X-Goog-FieldMask"] = ",".join(fields) if fields else self.DEFAULT_FIELDS
 
         url = os.path.join(self.base_url, "places:searchText")
-        try:
-            response = requests.post(url, headers=headers, json=json_data, timeout=10.0).json()
-            if not isinstance(response, dict):
-                log.print_fail(f"Could not search for {query}")
-                log.print_warn(response)
-                return {}
 
-            return response
-        except Exception as exception:  # pylint: disable=broad-except
-            log.print_fail(f"Could not search for {query}")
-            log.print_warn(exception)
-            raise exception
+        return call_api(url, headers, json_data)
+
+    def find_nearby_places_from_place_id(
+        self, place_id: str, fields: T.Optional[T.List[str]] = None
+    ) -> T.Dict[T.Any, T.Any]:
+        if fields is None:
+            fields = ADVANCED_FIELDS
+
+        headers = copy.deepcopy(self.HEADERS)
+
+        headers["X-Goog-FieldMask"] = ",".join(fields) if fields else self.DEFAULT_FIELDS
+
+        url = os.path.join(self.base_url, "places/{place_id}")
+
+        if self.verbose:
+            log.print_normal(f"Searching for nearby places from {place_id}")
+
+        return call_api(url, headers=headers)
 
     def search_location_radius(
         self,
